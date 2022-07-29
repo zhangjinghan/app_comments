8aaa9d2152c01a46de0baad28a2a7214d34bf7c7
jinghan <1355443601@qq.com>
2022-07-29 17:23:46 +0800

    删除jinghan文件夹

--- comments/comments/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from database.models import CommentsList
-from django.core import serializers
-
-
-def comments(request):
-    data = CommentsList.objects.all()
-    res = serializers.serialize('json', queryset=data)
-    return HttpResponse(res)
